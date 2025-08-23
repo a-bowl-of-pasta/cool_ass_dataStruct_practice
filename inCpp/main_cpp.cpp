19804c06145052a155e482916814a835fbb1dd0c
bowlOfPasta <ahernandez94476@gmail.com>
2025-08-22 20:39:44 -0700

    AI games V5.1
    I reworked the main menu a little

--- inCpp/main_cpp.cpp
+++ inCpp/main_cpp.cpp
@@ -5,35 +5,33 @@
 
 int main()
 {
-    std::cout << "this is a menue to test out the various data structures and some of their algorithms" <<std::endl; 
-    std::cout << "\n1.) snake\n2.) AI sliding puzzle solver\n3.) AI map directions\n4.) interactive grid world\n0) End Game"<<std::endl; 
-    
-    int choice; 
+    int choice = 0 ; 
+    do 
+    {
+        std::cout << "\nHello, I wanted to see if i could\nwrite data structures and\nalgorithms by hand." <<std::endl;
+        std::cout << "so here is a collection\nof games using AI - all \nhand written : data structs, games, and algs." << std::endl<<std::endl;  
+        std::cout << "1.) snake\n2.) sliding 8 puzzle\n3.) interactive grid world\n0.) End Game"<<std::endl; 
+        std::cout << "enter choice :: "; 
+        std::cin>>choice; 
 
-    std::cin>>choice; 
-    system("cls"); 
+        system("cls"); 
 
-    if(choice == 0)
-    {
-        return 0; 
-    }
-    else if(choice == 1)
-    {
-        playSnake(); 
-    }
-    else if(choice == 2)
-    {
-        play_eight_puzzle(); 
-    }
-    else if(choice == 3)
-    {
+        if(choice == 1)
+        {
+            playSnake(); 
+        }
+        else if(choice == 2)
+        {
+            play_eight_puzzle(); 
+        }
+        else if(choice == 3)
+        {
 
-    }
-    else if(choice == 4)
-    {
+        }
+  
+    } while(choice != 0);  
 
-    }
-    
+    std::cout << "hope you enjoyed at least\none of those games\n:)\n\t-the_tonester"<<std::endl<<std::endl;
 
     return 0; 
 }
